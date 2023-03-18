45013679a8c9ee404a19330ad1af285b6282dc9e
unknown <calrogerscal@gmail.com>
2023-03-18 08:39:50 +0200

    added authentication folder

---
+++ src/screens/authentication/relativesContacts/RelativesContacts.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+
+const RelativesContacts = () => {
+  return (
+    <View>
+      <Text>RelativesContacts</Text>
+    </View>
+  )
+}
+
+export default RelativesContacts
+
+const styles = StyleSheet.create({})
